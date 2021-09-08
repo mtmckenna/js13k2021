e23122a67779eed4e93ea7789d85a3b45ba44738
Matt McKenna <matt@mtmckenna.com>
2021-09-08 00:56:15 -0400

    PTO changes

---
+++ types.ts
@@ -0,0 +1,42 @@
+import { ProgramCache } from "./webgl-helpers";
+
+interface Animation {
+  startTime: number;
+  endTime: number;
+  startValue: number;
+  endValue: number;
+  currentValue: number;
+}
+
+export interface Camera {
+  props: Float32Array;
+}
+
+export interface Circle {
+  index: number;
+  radius: number;
+  color: Float32Array;
+  vel: Float32Array;
+  acc: Float32Array;
+  animation: Animation;
+}
+
+export interface GameProgramCache extends ProgramCache {
+  uniforms: {
+    uRes: WebGLUniformLocation | null;
+    uCircleProps: WebGLUniformLocation | null;
+    uCircleColorProps: WebGLUniformLocation | null;
+    uCameraProps: WebGLUniformLocation | null;
+    uTime: WebGLUniformLocation | null;
+    uBorder: WebGLUniformLocation | null;
+  };
+}
+
+export interface GameState {
+  started: boolean;
+  level: number;
+  gameOver: boolean;
+  levelWon: boolean;
+  dimensions: { width: number; height: number };
+  readyToTryAgainAt: number;
+}
